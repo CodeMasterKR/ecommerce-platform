175409196b6389287f1cf274d207fbdbe5f4962d
Kamron <ibrahimovkamronbek777@gmail.com>
2026-03-16 02:46:49 +0500

    feat: setup NestJS modules with PrismaService

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { CartModule } from './cart/cart.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ProductsModule, OrdersModule, CartModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
